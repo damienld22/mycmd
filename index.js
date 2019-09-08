42bdaa1afde6cb18f417137c486c671b262f6443
Damien Le Dantec <damien-le-dantec@hotmail.fr>
2019-09-08 23:45:26 +0200

    Add 'save' command

--- index.js
+++ index.js
@@ -2,15 +2,23 @@
  * Start file of the CLI
  */
 const program = require('commander');
+const process = require('process');
 const version = require('./package.json').version;
 const listCommands = require('./lib/listCommands');
+const saveCommand = require('./lib/saveCommand');
 
 program
   .version(version)
   .description('Basic CLI to save / list / search Unix commands')
   .option('-l, --list, List all commands')
+  .option('-s, --save, Save a command')
   .parse(process.argv);
 
 if (program.list) {
   listCommands();
+  process.exit(0);
+} else if (program.save) {
+  saveCommand();
+} else {
+  console.log('Unknow command');
 }
