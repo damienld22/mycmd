28fd58400021dacfd705cb7649f47ef39238c54a
Damien Le Dantec <damien-le-dantec@hotmail.fr>
2019-09-09 00:05:21 +0200

    Add 'delete' command

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const process = require('process');
 const version = require('./package.json').version;
 const listCommands = require('./lib/listCommands');
 const saveCommand = require('./lib/saveCommand');
+const deleteCommand = require('./lib/deleteCommand');
 
 program
   .version(version)
   .description('Basic CLI to save / list / search Unix commands')
   .option('-l, --list, List all commands')
   .option('-s, --save, Save a command')
+  .option('-d, --delete <name>, Delete a command')
   .parse(process.argv);
 
 if (program.list) {
@@ -19,6 +21,9 @@ if (program.list) {
   process.exit(0);
 } else if (program.save) {
   saveCommand();
+} else if (program.delete) {
+  deleteCommand(program.delete);
+  process.exit(0);
 } else {
   console.log('Unknow command');
 }
