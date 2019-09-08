78d966fcfc36ef69a49579e1336a4b5ab626f5b8
Damien Le Dantec <damien-le-dantec@hotmail.fr>
2019-09-08 23:15:54 +0200

    Add 'list' command

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
  */
 const program = require('commander');
 const version = require('./package.json').version;
+const listCommands = require('./lib/listCommands');
 
 program
   .version(version)
-  .description("Basic CLI to save / list / search Unix commands")
+  .description('Basic CLI to save / list / search Unix commands')
+  .option('-l, --list, List all commands')
   .parse(process.argv);
 
-
+if (program.list) {
+  listCommands();
+}
